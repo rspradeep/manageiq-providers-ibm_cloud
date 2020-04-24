57c289b536ec4d3dadfb8cb6f964e18552a6d56c
Isgandar Valizada <ivalizad@de.ibm.com>
2020-04-24 20:41:44 +0200

    added HW info parsing, CPU cores and memory

--- app/models/manageiq/providers/ibm_cloud_virtual_servers/inventory/parser/cloud_manager.rb
+++ app/models/manageiq/providers/ibm_cloud_virtual_servers/inventory/parser/cloud_manager.rb
@@ -1,20 +1,43 @@
 class ManageIQ::Providers::IbmCloudVirtualServers::Inventory::Parser::CloudManager < ManageIQ::Providers::Inventory::Parser
-
-  def parse
-  self.collector.vms().each do |vmi|
-		persister_instance = persister.vms.build(
+  
+  def instances
+	self.collector.vms().each do |instance|
+		vmi = {
 			:availability_zone => "",
-			:description       => "IBM Power Server",
-			:ems_ref           => vmi["pvmInstanceID"],
-			:flavor            => "",
-			:location          => "unknown",
-			:name              => vmi["serverName"],
-			:vendor			   => 'unknown',
-			:genealogy_parent  => "",
-			:connection_state  => "connected",
-			:raw_power_state   => vmi["status"],
-			:uid_ems           => vmi["pvmInstanceID"],
-		)
+            :description       => "IBM Cloud Server",
+            :ems_ref           => instance["pvmInstanceID"],
+            :flavor            => "",
+            :location          => "unknown",
+            :name              => instance["serverName"],
+            :vendor            => 'IBM',
+            :genealogy_parent  => "",
+            :connection_state  => "connected",
+            :raw_power_state   => instance["status"],
+            :uid_ems           => instance["pvmInstanceID"],
+		}
+
+		hardware =
+		{
+			:cpu_total_cores => Float(instance['processors']),
+			:memory_mb       => Integer(instance['memory']) * 1024,
+		}		
+
+		yield vmi, hardware if block_given?
+	end
+  end		
+ 
+  def parse
+		instances do |vmi, hardware| 
+			# saving general VMI information
+			ps_vmi = self.persister.vms.build(vmi)
+
+			# saving hardware information (CPU, Memory, etc.)
+			hardware[:vm_or_template] = ps_vmi
+			ps_hardware = self.persister.hardwares.build(hardware)
+
+			# saving network information
+			# TODO: coming soon
+		end
+		  	
 	end
   end
-end
