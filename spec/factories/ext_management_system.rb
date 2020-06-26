814cc829cfbff1ba44ae99b8cff9d7fbc3c145bf
Adam Grare <agrare@redhat.com>
2020-06-26 11:30:35 -0400

    Add a VCR based refresher spec test for EmsRefresh

---
+++ spec/factories/ext_management_system.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :ems_ibm_cloud_virtual_servers_cloud,
+          :aliases => ["manageiq/providers/ibm_cloud_virtual_servers/cloud_manager"],
+          :class => "ManageIQ::Providers::IbmCloudVirtualServers::CloudManager",
+          :parent => :ems_cloud
+end
