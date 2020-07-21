6fd19b2f48b3570633ef4a60de2cd790b15b47f3
Kuldip-Nanda <kuldip.nanda@ibm.com>
2020-07-21 02:05:19 -0400

    app/models/manageiq/providers/ibm_cloud_virtual_servers/net_control_api.rb
    	modified:   .travis.yml
    	modified:   app/models/manageiq/providers/ibm_cloud_virtual_servers/net_control_api.rb
    	modified:   app/models/manageiq/providers/ibm_cloud_virtual_servers/network_manager.rb
    	modified:   app/models/manageiq/providers/ibm_cloud_virtual_servers/network_manager/cloud_network.rb
    	modified:   app/models/manageiq/providers/ibm_cloud_virtual_servers/network_manager/cloud_subnet.rb
    	modified:   spec/vcr_cassettes/manageiq/providers/ibm_cloud_virtual_servers/cloud_manager/refresher.yml
    	spec/models/manageiq/providers/ibm_cloud_virtual_servers/cloud_manager/refresher_spec.rb

--- app/models/manageiq/providers/ibm_cloud_virtual_servers/net_control_api.rb
+++ app/models/manageiq/providers/ibm_cloud_virtual_servers/net_control_api.rb
@@ -6,13 +6,28 @@ class ManageIQ::Providers::IbmCloudVirtualServers::NetControlAPI
     @creds  = creds
   end
 
-  def del_network(network_id)
+  def del_subnet(network_id)
     response = RestClient.delete(
       "https://#{@creds[:region]}.power-iaas.cloud.ibm.com" \
       "/pcloud/v1/cloud-instances/#{@creds[:guid]}/networks/#{network_id}",
       'Authorization' => @creds[:token].get,
       'CRN'           => @creds[:crn],
-      'Accept'        => 'application/json'
+      'Accept'        => 'application/json',
+    )
+
+    JSON.parse(response.body)
+  end
+
+
+  def create_subnet(subnet)
+    response = RestClient.post(
+      "https://#{@creds[:region]}.power-iaas.cloud.ibm.com" \
+      "/pcloud/v1/cloud-instances/#{@creds[:guid]}/networks",
+      subnet.to_json,
+      'Authorization' => @creds[:token].get,
+      'CRN'           => @creds[:crn],
+      'Accept'        => 'application/json',
+      'Content-Type'  => 'application/json',
     )
 
     JSON.parse(response.body)
