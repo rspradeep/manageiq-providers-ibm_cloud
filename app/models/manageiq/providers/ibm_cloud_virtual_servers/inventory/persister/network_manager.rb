902ec4b90166fea0051c66b12f5dd7e9a44597db
Adam Grare <agrare@redhat.com>
2020-07-28 07:29:20 -0400

    Cleanup unused persister definitions

--- app/models/manageiq/providers/ibm_cloud_virtual_servers/inventory/persister/network_manager.rb
+++ app/models/manageiq/providers/ibm_cloud_virtual_servers/inventory/persister/network_manager.rb
@@ -1,6 +1,2 @@
 class ManageIQ::Providers::IbmCloudVirtualServers::Inventory::Persister::NetworkManager < ManageIQ::Providers::IbmCloudVirtualServers::Inventory::Persister
-  def initialize_inventory_collections
-    initialize_cloud_inventory_collections
-    initialize_network_inventory_collections
-  end
 end
--- app/models/manageiq/providers/ibm_cloud_virtual_servers/inventory/persister/storage_manager.rb
+++ app/models/manageiq/providers/ibm_cloud_virtual_servers/inventory/persister/storage_manager.rb
@@ -1,6 +1,2 @@
 class ManageIQ::Providers::IbmCloudVirtualServers::Inventory::Persister::StorageManager < ManageIQ::Providers::IbmCloudVirtualServers::Inventory::Persister
-  def initialize_inventory_collections
-    initialize_cloud_inventory_collections
-    initialize_storage_inventory_collections
-  end
 end
