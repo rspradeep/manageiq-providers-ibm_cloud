9385a34f865c2c49087dee916dbfcf079ac05806
Jay W. Carman <jwcarman@us.ibm.com>
2020-07-29 05:38:25 -0500

    Enable direct refresh of a NetworkManager and StorageManager
    Refresh of NetworkManager or StorageManager will result in a refresh of
    its sibling manager and parent CloudManager. The sibling manager needs
    to be defined relative to `manager.parent_manager`.

--- app/models/manageiq/providers/ibm_cloud_virtual_servers/inventory/persister/network_manager.rb
+++ app/models/manageiq/providers/ibm_cloud_virtual_servers/inventory/persister/network_manager.rb
@@ -1,2 +1,5 @@
 class ManageIQ::Providers::IbmCloudVirtualServers::Inventory::Persister::NetworkManager < ManageIQ::Providers::IbmCloudVirtualServers::Inventory::Persister
+  def storage_manager
+    manager.parent_manager.storage_manager
+  end
 end
