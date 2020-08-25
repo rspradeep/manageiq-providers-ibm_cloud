1c0c657f1c16f4355036a00ef91191a56ad6adbf
Jay W Carman <jay@carman.us.com>
2020-08-25 07:39:53 -0500

    WIP

--- spec/factories/vm.rb
+++ spec/factories/vm.rb
@@ -1,7 +1,11 @@
 FactoryBot.define do
-  factory :vm_ibm_cloud_powervs, :class => "ManageIQ::Providers::IbmCloud::PowerVirtualServers::CloudManager::Vm", :parent => :ems_ibm_cloud_power_virtual_servers_cloud do
-    location        { |x| "[storage] #{x.name}/#{x.name}.vmx" }
-    vendor          { "vm_Ibm_Cloud_Powervs" }
-    raw_power_state { "poweredOn" }
+  factory :vm_ibm_cloud_power_virtual_servers, :class => "ManageIQ::Providers::IbmCloud::PowerVirtualServers::CloudManager::Vm", :parent => :vm_cloud do
+    vendor { "ibm" }
+
+    trait :with_provider do
+      after(:create) do |x|
+        FactoryBot.create(:ems_ibm_cloud_power_virtual_servers_cloud, :vms => [x])
+      end
+    end
   end
 end
